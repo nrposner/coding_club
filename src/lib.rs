3150702e5c0de5631427c302f9f37d6e7ab632e3
nrposner <nicolasposner@gmail.com>
2025-07-22 16:04:52 -0400

    adding function signatures

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,34 @@ use ndarray::linalg::Dot;
 use ndarray::{Array2, ArrayBase, Axis, Ix2, LinalgScalar, OwnedRepr};
 use std::ops::AddAssign;
 
-/// Formats the sum of two numbers as string.
+// Define a custom type just to make this a bit more readable
+type Matrix<A> = ArrayBase<OwnedRepr<A>, Ix2>;
+
+// Define a Rust function for exponentiating a matrix
+// This function has never even heard of Python!
+pub fn matrix_power_rust<A>(
+    matrix: &Matrix<A>,     // <- type definitions like this are MANDATORY in Rust 
+    mut exponent: usize     // by default, all objects are immutable. We need to mark them `mut`
+) -> Matrix<A>              // to change them
+where
+    A: LinalgScalar,
+    Matrix<A>: Dot<Matrix<A>, Output = Matrix<A>>
+{ }
+
+// Define a pyfunction: this is a function written in Rust, which is designed to 
+// take inputs from and return outputs to Python
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+pub fn matrix_power(
+    // 1. Accept any Python object. A list of lists is expected
+    matrix_obj: &Bound<'_, PyAny>,  // <- Remember how type definitions are mandatory? 
+    exponent: usize,                // Well, dealing with a language which doesn't have
+) -> PyResult<Vec<Vec<f64>>> {      // strong types, we sometimes need a type that says 'I dunno :P'
+
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn coding_club(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(matrix_power, m)?)?;
     Ok(())
 }
